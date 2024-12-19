0480cb84dcbdf5d5c9a125c2f18ef17da7a4ef78
Theolaos <92591390+theolaos@users.noreply.github.com>
2024-12-19 22:17:48 +0200

    Fixes RendererMethods Syntax Error

--- src/params.py
+++ src/params.py
@@ -3,7 +3,7 @@ from .tleng2.components.camera import CameraCatcher
 
 
 def import_params_needed() -> None:
-    RendererMethods.import_scene_renderer_params_list({
+    RendererMethods.import_scene_renderer_params_dict({
         'free_roam':{
             'display' : RendererMethods.load_local_display_ratio(1/3),
             'camera' : 'camera0'
